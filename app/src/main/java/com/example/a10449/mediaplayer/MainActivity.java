766c8beba65a6fe6fc546b0f9aae2a2f112a1c52
jioper <1044991441@qq.com>
2018-12-07 14:40:17 +0800

    UI method call UI层方法调用

--- app/src/main/java/com/example/a10449/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/a10449/mediaplayer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a10449.mediaplayer;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.media.CamcorderProfile;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar mSeekBar;
     private Button mClose;
     private Button mplay;
+    private PlayerConnection mPlayerConnection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
         initView();
         //设置相关事件
         initEvent();
+        //开启服务
+        initService();
+        //绑定服务
+        initBindService();
+    }
+//开启服务
+    private void initService() {
+        startService(new Intent(this,PlayerService.class));
+    }
+//绑定服务
+    private void initBindService() {
+        Intent intent=new Intent(this,PlayerService.class);
+        if(mPlayerConnection==null)
+        {
+            mPlayerConnection=new PlayerConnection();
+        }
+        bindService(intent, (ServiceConnection) mPlayerConnection,BIND_AUTO_CREATE);
+    }
+    private class PlayerConnection implements ServiceConnection{
+        public void onServiceConnected(ComponentName name,IBinder service){
+
+        }
+        public void onServiceDisconnected(ComponentName name){
+
+        }
+
+
     }
     private void initEvent(){
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -54,5 +86,13 @@ public class MainActivity extends AppCompatActivity {
         mplay=(Button)this.findViewById(R.id.play_btn);
         mClose=(Button)this.findViewById(R.id.close_btn);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPlayerConnection != null) {
+            unbindService(mPlayerConnection);
+        }
     }
 }
---
+++ app/src/main/java/com/example/a10449/mediaplayer/PlayerService.java
@@ -0,0 +1,11 @@
+package com.example.a10449.mediaplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PlayerService extends Service {
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+}
