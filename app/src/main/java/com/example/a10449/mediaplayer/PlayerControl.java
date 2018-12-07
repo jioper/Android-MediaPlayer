2b809ca138b9dc69025878404ea45476e9670af1
jioper <1044991441@qq.com>
2018-12-07 14:07:44 +0800

    Add hint

--- app/src/main/java/com/example/a10449/mediaplayer/PlayerControl.java
+++ app/src/main/java/com/example/a10449/mediaplayer/PlayerControl.java
@@ -5,9 +5,14 @@ public interface PlayerControl {
     void RegisterViewController(PlayerViewControl viewControl);
     //取消接口通知的注册
     void unRegisterViewController();
+    //播放
     void play();
+    //暂停
     void pasuse();
+    //继续
     void resume();
+    //停止
     void stop();
+    //设置播放进度
     void seekTo(int seek);
 }
